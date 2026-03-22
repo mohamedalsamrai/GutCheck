74f0717cf35cb95f87f1b46dad48f6ba5c91c1e1
Konstantin Unruh <freaxmate@protonmail.com>
2026-03-22 20:10:40 +0100

    fix gh pages

--- astro-site/astro.config.mjs
+++ astro-site/astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://freaxmate.github.io/GutCheck/',
   base: '/GutCheck/',
-  integrations: [],
+  integrations: [tailwind()],
 });
